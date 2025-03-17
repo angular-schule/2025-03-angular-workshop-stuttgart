d1177b226538a6cecbc3631b64ba194976599faf
Ferdinand Malcher <ferdinand@malcher.media>
2025-03-17 09:51:27 +0100

    aufräumen

--- book-rating/src/main.ts
+++ book-rating/src/main.ts
@@ -4,37 +4,3 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
-
-
-///////////////
-
-export class Customer {
-
-  // Property
-  #age: number = 18;
-
-  constructor() {
-    this.#age = 20;
-  }
-
-  // Methode
-  #makeFoo(arg: number): string {
-    return 'dfgdf';
-  }
-}
-
-
-const myCustomer = new Customer();
-// myCustomer.makeFoo()
-
-
-
-
-const foo = arg => arg + 1;
-
-
-const result = foo(5);
-
-setTimeout(() => {
-
-}, 2000);
