c4f09bf1048e0ed8985d02e2ce240ca396956a87
Ferdinand Malcher <ferdinand@malcher.media>
2025-03-18 12:04:19 +0100

    Rating Min/Max

--- book-rating/src/app/books/shared/book-rating.service.ts
+++ book-rating/src/app/books/shared/book-rating.service.ts
@@ -7,6 +7,11 @@ import { Book } from './book';
 export class BookRatingService {
 
   rateUp(book: Book): Book {
+    // Early Exit
+    if (book.rating >= 5) {
+      return book;
+    }
+
     return {
       // Spread Operator
       ...book,
@@ -17,7 +22,8 @@ export class BookRatingService {
   rateDown(book: Book): Book {
     return {
       ...book,
-      rating: book.rating - 1
+      rating: Math.max(1, book.rating - 1)
+      // rating: book.rating <= 1 ? book.rating : book.rating - 1
     };
   }
 }
