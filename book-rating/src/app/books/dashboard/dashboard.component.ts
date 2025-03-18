eab1723f68e1f50578241ebae2da59c606b6c386
Ferdinand Malcher <ferdinand@malcher.media>
2025-03-18 14:50:20 +0100

    HTTP

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { Book } from '../shared/book';
 import { BookComponent } from '../book/book.component';
 import { DatePipe } from '@angular/common';
 import { BookRatingService } from '../shared/book-rating.service';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -16,30 +17,19 @@ export class DashboardComponent {
   readonly myDate = signal(Date.now());
 
   #rs = inject(BookRatingService);
+  #bs = inject(BookStoreService);
 
   constructor() {
+    this.#bs.getAll().subscribe(receivedbooks => {
+      this.books.set(receivedbooks);
+    });
+
+
     setInterval(() => {
       this.myDate.set(Date.now())
     }, 1000);
 
-    this.books.set([
-      {
-        isbn: '456',
-        title: 'Angular',
-        description: 'Grundlagen und mehr',
-        price: 42.9,
-        rating: 5,
-        authors: ['FM', 'DK', 'JH']
-      },
-      {
-        isbn: '123',
-        title: 'Vue.js',
-        description: 'Das grüne Framework',
-        price: 32.9,
-        rating: 3,
-        authors: ['FD']
-      },
-    ]);
+
   }
 
   doRateUp(book: Book) {
---
+++ book-rating/src/app/books/shared/book-store.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Book } from './book';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookStoreService {
+
+  #apiUrl = 'https://api.angular.schule';
+  #http = inject(HttpClient);
+
+  getAll(): Observable<Book[]> {
+    return this.#http.get<Book[]>(`${this.#apiUrl}/books`); // this.#apiUrl + '/books'
+  }
+
+  getSingle(isbn: string): Observable<Book> {
+    return this.#http.get<Book>(`${this.#apiUrl}/books/${isbn}`)
+  }
+
+  create(book: Book): Observable<Book> {
+    return this.#http.post<Book>(`${this.#apiUrl}/books`, book);
+  }
+
+  search(term: string): Observable<Book[]> {
+    return this.#http.get<Book[]>(`${this.#apiUrl}/books/search/${term}`);
+  }
+}
