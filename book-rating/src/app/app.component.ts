f72049aca6dfc3140ee8ec5235f8ab11117a8f10
Ferdinand Malcher <ferdinand@malcher.media>
2025-03-18 14:50:16 +0100

    Observables

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CounterComponent } from './counter/counter.component';
 import { DashboardComponent } from './books/dashboard/dashboard.component';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,5 +13,53 @@ import { DashboardComponent } from './books/dashboard/dashboard.component';
 export class AppComponent {
   title = 'Book Rating';
   foo = 5;
+
+  constructor() {
+    // Producer: generiert Werte
+    function producer(sub: any) {
+      const result = Math.random();
+      sub.next(result);
+      const result2 = Math.random();
+      sub.next(result2);
+
+      setTimeout(() => sub.next(5), 2000)
+      setTimeout(() => sub.next(10), 3000)
+      setTimeout(() => sub.complete(), 4000)
+    }
+
+    // Observer: empfängt die Werte aus dem Datenstrom
+    const obs = {
+      next: (e: number) => console.log(e),
+      error: (err: any) => console.error(err),
+      complete: () => console.log('FERTIG'),
+    }
+
+    // Observable: Schnittstelle zwischen Producer und Observer
+    // $: Finnische Notation
+    const myObs$ = new Observable<number>(sub => {
+      const result = Math.random();
+      sub.next(result);
+      const result2 = Math.random();
+      sub.next(result2);
+
+      setTimeout(() => sub.next(5), 2000)
+      setTimeout(() => sub.next(10), 3000)
+      setTimeout(() => sub.complete(), 4000)
+    });
+
+    // producer(obs);
+    // myObs$.subscribe(obs);
+    myObs$.subscribe(e => console.log(e))
+
+    myObs$.subscribe({
+      next: e => console.log(e)
+    })
+
+    /*producer({
+      next: (e: number) => console.log('X', e),
+      error: () => {},
+      complete: () => {}
+    });*/
+  }
 
 }
