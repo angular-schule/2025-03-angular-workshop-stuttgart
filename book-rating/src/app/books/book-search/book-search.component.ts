22427abf634f4d3f6e1bd21f37b6513383bcef4b
Ferdinand Malcher <ferdinand@malcher.media>
2025-03-20 16:33:56 +0100

    Typeahead-Suche

--- book-rating/src/app/books/book-search/book-search.component.ts
+++ book-rating/src/app/books/book-search/book-search.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { BookStoreService } from '../shared/book-store.service';
+import { debounceTime, filter, switchMap } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'app-book-search',
@@ -8,9 +12,19 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './book-search.component.scss'
 })
 export class BookSearchComponent {
+  #bs = inject(BookStoreService);
   searchControl = new FormControl('', { nonNullable: true });
 
+  readonly results = toSignal(
+    this.searchControl.valueChanges.pipe(
+      debounceTime(300),
+      filter(term => term.length >= 3),
+      switchMap(term => this.#bs.search(term)),
+    ),
+    { initialValue: [] }
+  );
+
   constructor() {
-    this.searchControl.valueChanges.subscribe(e => console.log(e));
+
   }
 }
